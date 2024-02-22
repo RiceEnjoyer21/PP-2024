606f5df4ba50b3ab612547a0a3e616b01879359f
RiceEnjoyer21 <rice.old21@gmail.com>
2024-02-22 20:36:08 +0600

    update

--- Lab5/regex/4.py
+++ Lab5/regex/4.py
@@ -4,6 +4,6 @@ file = open("row.txt", "r", encoding="utf8")
 
 text = file.read()
 
-x = re.findall("[А-Я][а-я]+", text)                  #x = re.findall("[A-Z][a-z]+", text)
+x = re.findall("[А-Я][а-я]+", text)                  #x = re.findall("[A-Z][a-z]+", text) originally
 
 print(x)
--- Lab5/regex/test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-text = str(input())
-
-x = re.sub(r"_([a-z])", lambda match: match.group(1).upper(), text)
-
-print(x)
