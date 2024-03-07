e2757c5716593c1021786697686dd3c54bfe1fcc
RiceEnjoyer21 <rice.old21@gmail.com>
2024-03-07 12:49:42 +0500

    have done a week ago, forgot to push (-_-)

---
+++ Lab6/builtin-functions/2.py
@@ -0,0 +1,16 @@
+word = str(input())
+
+def count(word):
+    a = 0
+    b = 0
+    for l in word:
+        if l.isupper():
+            a += 1
+    for u in word:
+        if u.islower():
+            b += 1
+    print(a)
+    print(b)
+
+count(word)
+
